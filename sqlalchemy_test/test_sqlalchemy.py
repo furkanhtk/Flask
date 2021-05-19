88b19cf6dd9b8bb4739b77e08ae3a468ae9e4ea6
Furkan Aslan HATIK <furkanhtk>
2021-05-20 01:31:09 +0300

    SQLalchemy files moved to Test Folder

--- sqlalchemy_test/test_sqlalchemy.py
+++ sqlalchemy_test/test_sqlalchemy.py
@@ -15,9 +15,18 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Book(Base):
+    __tablename__ = 'book'
 
+    id = Column(Integer, primary_key=True)
+    Frequency = Column(String(250), nullable=False)
+    Power = Column(String(250), nullable=False)
+
+
+# creates a create_engine instance at the bottom of the file
 
 engine = create_engine('sqlite:///parameters_database.db')
 
 Base.metadata.create_all(engine)
+
 
